0e8c13a871e92bb2d4196d76281f6b1352766782
Yu Lin <linyu3294@gmail.com>
2020-01-11 12:43:28 -0500

    Adding spring framework components for java server

---
+++ src/test/java/com/example/wbdbonlinesp20javalinyu3294/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdbonlinesp20javalinyu3294;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
