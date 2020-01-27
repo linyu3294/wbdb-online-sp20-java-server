cbfc265a851014b1356d3e931e9efb70d6b77ff1
Yu Lin <linyu3294@gmail.com>
2020-01-26 22:51:09 -0500

    Checkpoint- finished a working copy or update users and add users

--- src/main/webapp/user-admin/user-admin.component.client.js
+++ src/main/webapp/user-admin/user-admin.component.client.js
@@ -2,19 +2,26 @@
 (function () {
     let searchbtn = $("#search")
     let userService = new AdminUserServiceClient()
+
     let users=[]
     let userTable = $("#user_table")
-    let userList = $("#user-placeholder")
+    let userList = $("#user-item")
+    let usernameFld = $("#username")
+    let passwordFld = $("#password")
+    let firstnameFld = $("#firstname")
+    let lastnameFld = $("#lastname")
+    let roleFld = $("#role")
+
+
 
     console.log("hello DOM")
 
 
-    userList.empty()
-
     const renderUsers = () => {
+        userTable.empty()
+
         for (let i =0; i<users.length; i++){
             //Renders the row that appears in browswer
-
             console.log(users[i].username)
             console.log(users[i].password)
             console.log(users[i].firstname)
@@ -22,7 +29,7 @@
             console.log(users[i].role)
 
             userTable.append(
-                '<tr>' +
+                '<tr id="user-item">' +
                 '<td>' + i + '</td>' +
                 '<td>' + users[i].username  + '</td>' +
                 '<td>' + users[i].password  + '</td>' +
@@ -33,22 +40,44 @@
                 '<td>' + '</td>' +
                 '<td> <i class="fa fa-times"></i> </td>' +
                 '<td>' + '</td>' +
-                '<td> <i class="fa fa-times"></i> </td>' +
+                '<td> <i class="fa fa-pencils"></i> </td>' +
                 '</tr>'
             )
+            userList.append()
         }
     }
-    renderUsers()
 
 
-    const findAllUsers = () =>
+
+    const createUser = () => {
+        const username = usernameFld.val()
+        const password = passwordFld.val()
+        const firstname = firstnameFld.val()
+        const lastname = lastnameFld.val()
+        const role = roleFld.val()
+        console.log(userList);
+        userService.createUser({username: username,
+                                     password: password,
+                                     firstname:firstname,
+                                     lastname:lastname,
+                                     role:role})
+        .then(newUser => {
+            findAllUsers()
+        })
+    }
+    searchbtn.click(createUser)
+
+
+    const findAllUsers = () => {
         userService.findAllUsers()
             .then((theUsers) => {
                 users = theUsers
                 console.log(users)
                 renderUsers()
             })
+    }
     findAllUsers()
 
-    searchbtn.click(findAllUsers)
+
+
 })();
